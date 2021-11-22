e7a1ec621adaf6c7bc29e8da1a57767b1687395b
Nicolas <nicolas.soares.gomes@gmail.com>
2021-11-22 10:24:39 -0300

    new logic in entry point

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,43 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
-import express, { Express } from 'express';
+import express, { Application } from 'express';
+import httpContext from 'express-http-context';
 import logger from 'morgan';
 import path from 'path';
 
-import Route from './routes/index.routes';
+import 'express-async-errors';
 
 import './database/postgres';
+
+import Route from './routes/index.routes';
 
 dotenv.config();
 
 export class App {
-    public app: Express;
-
-    constructor() {
+    constructor(private app: Application) {
         this.app = express();
         this.middlewares();
         this.routes();
     }
 
-    middlewares() {
-        this.app.use(cors());
+    private middlewares() {
+        this.app.use(
+            cors({
+                origin: '*',
+            })
+        );
         this.app.use(logger('dev'));
         this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.static(path.join(__dirname, 'src/public')));
+        this.app.use(httpContext.middleware);
     }
 
-    routes() {
+    private routes() {
         this.app.use(Route.router);
+    }
+
+    public getApp() {
+        return this.app;
     }
 }
