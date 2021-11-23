961936147a1011f352462a4250c7e41d3f9007ae
Nicolas <nicolas.soares.gomes@gmail.com>
2021-11-23 00:13:38 -0300

    new model bin start

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -7,45 +7,68 @@ import path from 'path';
 
 import { App } from '../app';
 
-const serverApp = new App().getApp();
+class Server {
+    private serverApp;
+    private port;
+    private portTls;
+    private server;
+    private serverSsl;
 
-const port = process.env.PORT || '8080';
-const portTls = process.env.PORT_TLS || '465';
-
-const server = http.createServer(serverApp);
-const serverSsl = https.createServer(
-    {
-        key: fs.readFileSync(path.join(__dirname, '../certificate/selfsigned.key'), 'utf-8'),
-        cert: fs.readFileSync(path.join(__dirname, '../certificate/selfsigned.crt'), 'utf-8'),
-    },
-    serverApp
-);
-
-if (cluster.isPrimary) {
-    const number_of_cpus = os.cpus().length;
-
-    console.log(`\nMaster ${process.pid} is running\n`);
-    console.log(`\nForking Server for ${number_of_cpus} CPUs\n`);
-
-    for (let index = 0; index < number_of_cpus; index++) {
-        cluster.fork();
+    constructor() {
+        this.cluster();
+        this.serverApp = new App().getApp();
+        this.port = process.env.PORT || '8080';
+        this.portTls = process.env.PORT_TLS || '465';
+        this.createServer(this.serverApp);
+        this.createServerTls(this.serverApp);
     }
 
-    cluster.on('exit', (worker, code) => {
-        if (code !== 0 && !worker.exitedAfterDisconnect) {
-            console.log(`\nWorker ${worker.process.pid} died\n`);
-            cluster.fork();
-        }
-    });
-} else {
-    console.log(`\nProcess id: ${process.pid}\n`);
+    private createServer(app) {
+        this.server = http.createServer(app);
+    }
 
-    setTimeout(() => {
-        server.listen(port, () =>
-            process.stdout.write(`\n SERVER HTTP RUNNING IN PORT: ${port}\nhttp://localhost:${port} \n\n`)
+    private createServerTls(app) {
+        this.serverSsl = https.createServer(
+            {
+                key: fs.readFileSync(path.join(__dirname, '../certificate/selfsigned.key'), 'utf-8'),
+                cert: fs.readFileSync(path.join(__dirname, '../certificate/selfsigned.crt'), 'utf-8'),
+            },
+            app
         );
-        serverSsl.listen(portTls, () =>
-            process.stdout.write(`\n SERVER HTTPS RUNNING IN PORT: ${portTls}\nhttps://localhost:${portTls}\n\n`)
-        );
-    }, 1000);
+    }
+
+    private cluster() {
+        if (cluster.isPrimary) {
+            const number_of_cpus = os.cpus().length;
+
+            console.log(`\nMaster ${process.pid} is running\n`);
+            console.log(`\nForking Server for ${number_of_cpus} CPUs\n`);
+
+            for (let index = 0; index < number_of_cpus; index++) {
+                cluster.fork();
+            }
+
+            cluster.on('exit', (worker, code) => {
+                if (code !== 0 && !worker.exitedAfterDisconnect) {
+                    console.log(`\nWorker ${worker.process.pid} died\n`);
+                    cluster.fork();
+                }
+            });
+        } else {
+            console.log(`\nProcess id: ${process.pid}\n`);
+
+            setTimeout(() => {
+                this.server.listen(this.port, () =>
+                    process.stdout.write(`\n SERVER HTTP RUNNING IN PORT: ${this.port}\nhttp://localhost:${this.port} \n\n`)
+                );
+                this.serverSsl.listen(this.portTls, () =>
+                    process.stdout.write(
+                        `\n SERVER HTTPS RUNNING IN PORT: ${this.portTls}\nhttps://localhost:${this.portTls}\n\n`
+                    )
+                );
+            }, 1000);
+        }
+    }
 }
+
+new Server();
