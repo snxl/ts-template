0266c89b56d6bc0f4cb2a518b888067768fc0a82
Nicolas <nicolas.soares.gomes@gmail.com>
2021-12-02 21:09:17 -0300

    validate fields

--- src/application/controller/user/createUserSchema.ts
+++ src/application/controller/user/createUserSchema.ts
@@ -1,15 +1,31 @@
+import regex from '@src/shared/config/regex';
+
 import BaseSchema from '../baseSchema';
 
 class CreateUserSchema extends BaseSchema {
+    readonly regexEmail: RegExp;
+
     constructor(body: any) {
         super();
         this.body = body;
 
-        const fields = ['name', 'email', 'password', 'passwordConfirm'];
+        const fields: Array<string> = ['name', 'email', 'password', 'passwordConfirm'];
 
         for (const field of fields) {
             if (this.body && !this.body[field]) {
                 this.errors.push(`${field} is empty`);
+            }
+
+            if (field === fields[1] && !regex.email.test(this.body[field])) {
+                this.errors.push(`${field} must be an email`);
+            }
+
+            if (field === fields[2] && this.body[field].length < 8) {
+                this.errors.push(`${field} must contain eight characters`);
+            }
+
+            if (field === fields[3] && this.body[field] !== this.body[fields[2]]) {
+                this.errors.push(`${field} must be the same as the password`);
             }
         }
     }
---
+++ src/domain/repositories/IRegexRepository.ts
@@ -0,0 +1,3 @@
+export default interface IRegexp {
+    email: RegExp;
+}
